a64c38b02068373a928f2991e6e68bec02d9f33f
Shane Sarnac <shane.sarnac@colorado.edu>
2016-03-11 13:30:29 -0700

    Created basic batting model file with initial data read in

---
+++ Batting_Model.R
@@ -0,0 +1,7 @@
+
+# Collaborators: Shane Sarnac, Ryan Smith, Zachary Collins
+# Date: March 11, 2016
+# Summary: This code aims to find trends and develop a model to predict hitters' performances. 
+
+batters = read.csv(file = "Baseball/Batting.csv", header = TRUE)
+
